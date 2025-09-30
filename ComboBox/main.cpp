644a771ad8edf090cb729a1bab0ed3c6b880947d
Ada <Ada@PROBOOK>
2025-09-30 20:12:38 +0700

    the first lesson DONE

--- ComboBox/main.cpp
+++ ComboBox/main.cpp
@@ -35,7 +35,14 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			INT i = SendMessage(hCombo, CB_GETCURSEL, 0, 0);
 			SendMessage(hCombo, CB_GETLBTEXT, i, (LPARAM)sz_buffer);
 			CHAR sz_message[SIZE] = {};
-			sprintf(sz_message, "Вы выбрали вариант N%i со значением '%s'", i, sz_buffer);
+			if (i != -1)
+			{
+				sprintf(sz_message, "Вы выбрали вариант N%i со значением '%s'", i, sz_buffer);
+			}
+			else
+			{
+				sprintf(sz_message, "Выберите значение", i, sz_buffer);
+			}
 			MessageBox(hwnd, sz_message, "Info", MB_OK | MB_ICONINFORMATION);
 		}
 			break;
