251c8e422be11cfb9806e4b3fa085fb4f519ad42
Ada <Ada@PROBOOK>
2025-10-18 17:45:14 +0700

    calc DONE

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -1,5 +1,11 @@
-﻿#include <Windows.h>
+﻿
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include <Windows.h>
 #include"resource.h"
+#include<float.h>
+
+//CONST INT SIZE = 256;
 
 CONST CHAR g_sz_CLASS_NAME[] = "Calc_CPU_411";
 
@@ -88,14 +94,19 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR plCmdLine, IN
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+	static double a = DBL_MIN;
+	static double b = DBL_MIN;
+	static INT operation = 0;
+	static BOOL input = false;
+	static BOOL input_operation = false;
 	switch (uMsg)
 	{
 	case WM_CREATE:
 	{
 		HWND hEdit = CreateWindowEx
 		(
-			NULL, "Edit", "",
-			WS_CHILD | WS_VISIBLE | WS_BORDER,
+			NULL, "Edit", "0",
+			WS_CHILD | WS_VISIBLE | WS_BORDER | ES_RIGHT,
 			10, 10,
 			g_i_SCREEN_WIDTH, g_i_SCREEN_HEIGHT,
 			hwnd,
@@ -109,7 +120,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		{
 			for (int j = 0; j < 3; j++)
 			{
-				sz_digit[0] = digit++ + '0';
+				sz_digit[0] = digit + '0';
 				CreateWindowEx
 				(
 					NULL, "Button", sz_digit,
@@ -122,6 +133,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 					GetModuleHandle(NULL),
 					NULL
 				);
+				digit++;
 			}
 
 		}
@@ -200,6 +212,79 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	}
 		break;
 	case WM_COMMAND:
+	{
+		CONST INT SIZE = 256;
+		CHAR sz_display[SIZE] = {};
+		CHAR sz_digit[2] = {};
+		HWND hEdit = GetDlgItem(hwnd, IDC_EDIT);
+		SendMessage(hEdit, WM_GETTEXT, SIZE, (LPARAM)sz_display);
+		if (LOWORD(wParam) >= IDC_BUTTON_0 && LOWORD(wParam) <= IDC_BUTTON_9)
+		{
+			if (input_operation) 
+				sz_display[0] = 0;
+			sz_digit[0] = LOWORD(wParam) - IDC_BUTTON_0 + 48;
+			if (strcmp(sz_display, "0"))
+				strcat(sz_display, sz_digit);
+			else
+				strcpy(sz_display, sz_digit);
+			SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)sz_display);
+			input = TRUE;
+			input_operation = FALSE;
+		}
+		if (LOWORD(wParam) == IDC_BUTTON_POINT && strchr(sz_display, '.') == NULL)
+		{
+			
+				strcat(sz_display, ".");
+				SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)sz_display);
+			
+		}
+		if (LOWORD(wParam) == IDC_BUTTON_BSP)
+		{
+			if (strlen(sz_display) == 1)
+				sz_display[0] = '0';
+			else 
+				sz_display[strlen(sz_display) - 1] = 0;
+			SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)sz_display);
+		}
+		if (LOWORD(wParam) == IDC_BUTTON_CLR)
+		{
+			a = b = DBL_MIN;
+			operation = 0;
+			input = input_operation = FALSE;
+			SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)"0");
+		}
+		if (LOWORD(wParam) >= IDC_BUTTON_PLUS && LOWORD(wParam) <= IDC_BUTTON_SLASH)
+		{
+			//if (a == DBL_MIN)
+			//	a = atof(sz_display);
+			//else
+			//	b = atof(sz_display);
+			if (input)
+			{
+				(a == DBL_MIN ? a : b) = atof(sz_display);
+				SendMessage(hwnd, WM_COMMAND, LOWORD(IDC_BUTTON_EQUAL), 0);
+			}
+			input = FALSE;
+			if(input_operation)SendMessage(hwnd, WM_COMMAND, LOWORD(IDC_BUTTON_EQUAL), 0);
+			operation = LOWORD(wParam);
+			input_operation = TRUE;
+		}
+		if (LOWORD(wParam) == IDC_BUTTON_EQUAL)
+		{
+			if (input) b = atof(sz_display);
+			switch (operation)
+			{
+			case IDC_BUTTON_PLUS: a += b; break;
+			case IDC_BUTTON_MINUS: a -= b; break;
+			case IDC_BUTTON_ASTER: a *= b; break;
+			case IDC_BUTTON_SLASH: a /= b; break;
+			}
+			input_operation = FALSE;
+			input = FALSE;
+			sprintf(sz_display, "%g", a);
+			SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)sz_display);
+		}
+	}
 		break;
 	case WM_DESTROY:
 		PostQuitMessage(0);
